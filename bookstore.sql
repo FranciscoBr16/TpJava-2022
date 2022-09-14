a3c34831cd187c2a1beb5bf895d2765c1fb14e06
FranciscoBBruno <franciscobruno2123@gmail.com>
2022-09-14 10:48:01 -0300

    Eliminacion de archivos

--- bookstore.sql
+++ bookstore.sql
@@ -1,10 +1,18 @@
 CREATE DATABASE  IF NOT EXISTS `bookstore` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bookstore`;
+<<<<<<< HEAD
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: bookstore
 -- ------------------------------------------------------
 -- Server version	8.0.28
+=======
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bookstore
+-- ------------------------------------------------------
+-- Server version	8.0.25
+>>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -38,9 +46,38 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+<<<<<<< HEAD
 INSERT INTO `categoria` VALUES (1,'Ficción');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
+=======
+INSERT INTO `categoria` VALUES (2,'Acción '),(1,'Ficción');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `editorial`
+--
+
+DROP TABLE IF EXISTS `editorial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `editorial` (
+  `ideditorial` int NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`ideditorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `editorial`
+--
+
+LOCK TABLES `editorial` WRITE;
+/*!40000 ALTER TABLE `editorial` DISABLE KEYS */;
+/*!40000 ALTER TABLE `editorial` ENABLE KEYS */;
+UNLOCK TABLES;
+>>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +88,8 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+<<<<<<< HEAD
 -- Dump completed on 2022-09-14 10:14:00
+=======
+-- Dump completed on 2022-09-14 10:28:21
+>>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
