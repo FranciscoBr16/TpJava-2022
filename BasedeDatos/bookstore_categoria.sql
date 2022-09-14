3c2e04ba294ccfac624b1a33c1d25efd65ee2b45
santiauat <santiauat@hotmail.com>
2022-09-14 11:06:25 -0300

    nueva BaseDeDatos

--- BasedeDatos/bookstore_categoria.sql
+++ BasedeDatos/bookstore_categoria.sql
@@ -1,24 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `bookstore` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bookstore`;
-<<<<<<< HEAD
-<<<<<<< HEAD
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: bookstore
--- ------------------------------------------------------
--- Server version	8.0.28
-=======
-=======
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
 -- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bookstore
 -- ------------------------------------------------------
 -- Server version	8.0.25
-<<<<<<< HEAD
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
-=======
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -40,9 +26,8 @@ DROP TABLE IF EXISTS `categoria`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria` (
   `idcategoria` int NOT NULL,
-  `descripcion` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idcategoria`),
-  UNIQUE KEY `categoriacol_UNIQUE` (`descripcion`)
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcategoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,44 +37,9 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-<<<<<<< HEAD
-<<<<<<< HEAD
-INSERT INTO `categoria` VALUES (1,'Ficción');
+INSERT INTO `categoria` VALUES (1,'Ficcion'),(2,'Accion');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
-=======
-=======
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
-INSERT INTO `categoria` VALUES (2,'Acción '),(1,'Ficción');
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `editorial`
---
-
-DROP TABLE IF EXISTS `editorial`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `editorial` (
-  `ideditorial` int NOT NULL,
-  `descripcion` varchar(45) NOT NULL,
-  PRIMARY KEY (`ideditorial`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `editorial`
---
-
-LOCK TABLES `editorial` WRITE;
-/*!40000 ALTER TABLE `editorial` DISABLE KEYS */;
-/*!40000 ALTER TABLE `editorial` ENABLE KEYS */;
-UNLOCK TABLES;
-<<<<<<< HEAD
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
-=======
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -100,16 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
--- Dump completed on 2022-09-14 10:14:00
-=======
--- Dump completed on 2022-09-14 10:28:21
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
-=======
--- Dump completed on 2022-09-14 10:28:21
->>>>>>> 164c8bb3e694c19b1d3692b10c2ef232e8b9c18d
-=======
--- Dump completed on 2022-09-14 10:56:26
->>>>>>> f47a1d45a33fc2443e7f99aef14833199db058f2
+-- Dump completed on 2022-09-14 11:04:57
