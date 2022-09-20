d956e561a3b361fce3421eb07dc92a720a130acb
santiauat <santiauat@hotmail.com>
2022-09-20 15:37:24 -0300

    agrego nro tomo en manga

--- Proyecto/bookstore/src/main/java/entities/Manga.java
+++ Proyecto/bookstore/src/main/java/entities/Manga.java
@@ -1,5 +1,14 @@
 package entities;
 
 public class Manga extends Libro{
+private int nro_tomo;
+
+public int getNro_tomo() {
+	return nro_tomo;
+}
+
+public void setNro_tomo(int nro_tomo) {
+	this.nro_tomo = nro_tomo;
+}
 
 }
